94aeb109ecef4a210a1caef49d1462640929348d
Vladimir <freshcode.yt@gmail.com>
2021-02-03 15:51:02 +0200

    Add promise creation

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const p = fetch('./data.json')
-  .then((res) => {
-    console.log(res)
-    return res.json(); // <- promise
+'use strict';
+
+const promiseShchrodinger = new Promise(executor);
+
+function executor(resolve, reject) {
+  const catsFate = Math.random();
+
+  if (catsFate >= 0.5) {
+    resolve('cat is alive');
+  } else {
+    reject('dead cat');
+  }
+}
+
+promiseShchrodinger
+  .then((catVerdict) => {
+    console.log(catVerdict);
   })
-  .then((result) => {
-    console.log(result);
+  .catch((err) => {
+    console.error(err);
   });
