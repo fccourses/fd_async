61826e6e888152662cda2a95f3d6422ed8db593f
Vladimir <freshcode.yt@gmail.com>
2021-02-03 15:07:11 +0200

    Add promise example

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-'use strict';
+const promise = fetch('./data.json');
 
-/*
- Создать объект с 3 свойствами(number, string, undefined), 1 метод
- Сериализовать JSON.stringify
- Вывести в консоль результат
- Десериализовать JSON.parse
- Посмотреть на результат
- */
+const promise2 = promise.then(
+  (response) => {
+    const promiseJson = response.json();
+    return promiseJson;
+  },
+  (err) => {
+    console.log('error: ', err);
+  }
+);
 
-const obj = {
-  firstName: 'Vlad',
-  hostname: 'facebook.com',
-  link: 'https://google.com',
-  getName() {},
-  test: undefined,
-};
-
-/* JSON - JavaScript Object Notation */
-
-const serializedObject = JSON.stringify(obj);
-console.log(serializedObject);
-
-const deserializedObject = JSON.parse(serializedObject);
-console.log(deserializedObject);
+promise2.then((data) => {
+  console.log(data);
+});
