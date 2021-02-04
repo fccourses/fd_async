bf126cda432bd5470748cedfcb2f3260b8a90d98
Vladimir <freshcode.yt@gmail.com>
2021-02-04 14:40:41 +0200

    Add finally()

--- index.js
+++ index.js
@@ -2,23 +2,21 @@
 
 console.log('1');
 
-const p1 = fetch('./data.json');
-
-console.log('2', p1);
-
-const p2 = p1.then((response) => {
-  console.log('5');
-  console.log(response);
-  const jsonPromise = response.json();
-  return jsonPromise;
-});
-
-console.log('3', p2);
-
-const p3 = p2.then((data) => {
-  console.log('6');
-  console.log(data);
-  return 5;
-});
-
-console.log('4', p3);
+fetch('./data.json')
+  .then((response) => {
+    console.log('5');
+    console.log(response);
+    const jsonPromise = response.json();
+    return jsonPromise;
+  })
+  .then((data) => {
+    console.log('6');
+    console.log(data);
+    return 5;
+  })
+  .catch((err) => {
+    console.log('ERROR WITH PROMISE',err);
+  })
+  .finally(()=>{
+    console.log('fetch ended')
+  })
